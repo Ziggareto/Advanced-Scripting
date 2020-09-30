822148facdfeebe627742da8887af6fd03748a27
Benjamin Dayan <benjamin@panakeia.ai>
2020-09-30 23:24:14 +0100

    Add first enemy prefab which moves in circles

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,31 @@
+﻿
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    
+    public float horizontalBound = 10f;
+    public float verticalBound = 10f;
+    public float elapsedTime = 0f;
+    [Tooltip("of the circle on which the Enemey moves")]
+    public float radius = 4f; [SerializeField]
+    private Vector2 startingPosition;
+    
+     // Start is called before the first frame update
+    void Start()
+    {
+        Transform transform = gameObject.GetComponent<Transform>();
+        transform.position = new Vector2( 
+            Random.Range(-horizontalBound, horizontalBound),
+             Random.Range(-verticalBound, verticalBound));
+        startingPosition = transform.position;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        elapsedTime = elapsedTime + Time.deltaTime;
+        transform.position = startingPosition + new Vector2(radius * Mathf.Sin( elapsedTime), radius*Mathf.Cos(elapsedTime));
+    }
+}
